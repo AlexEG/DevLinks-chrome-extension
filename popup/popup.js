4c1c67d803be1c768ec454a707080fabd3ef434f
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-08-11 19:26:30 +0300

    v1.0.0

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,9 @@
 const navItems = document.querySelectorAll(".nav-item");
 
+// const Docs = document.querySelector("#main > #docs");
+// const YouTube = document.querySelector("#main > #youtube");
+// const Other = document.querySelector("#main > #other");
+
 navItems.forEach(navItemHighlight);
 
 function navItemHighlight(item) {
@@ -8,5 +12,32 @@ function navItemHighlight(item) {
       item.classList.remove("border-b-2");
     });
     e.target.classList.add("border-b-2");
+
+    SectionHideShowFunc(e);
   });
+}
+
+function SectionHideShowFunc(e) {
+  if (e.target.innerText === "Docs") {
+    hide("youtube");
+    hide("other");
+    show("docs");
+  } else if (e.target.innerText === "YouTube") {
+    hide("docs");
+    hide("other");
+    show("youtube");
+  } else {
+    hide("docs");
+    hide("youtube");
+    show("other");
+  }
+}
+
+function hide(section) {
+  document.querySelector(`#main > #${section}`).classList.remove("flex");
+  document.querySelector(`#main > #${section}`).classList.add("hidden");
+}
+function show(section) {
+  document.querySelector(`#main > #${section}`).classList.remove("hidden");
+  document.querySelector(`#main > #${section}`).classList.add("flex");
 }
